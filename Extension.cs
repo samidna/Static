82c69073e8e6cfde477afcf19c41c9d5b689d767
samidna <samidna@code.edu.az>
2024-04-04 18:46:10 +0400

    created new project

---
+++ Extension.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp7;
+
+public static class Extension
+{
+    public static void IsPrime(this int number)
+    {
+        if (number == 1) Console.WriteLine("Number is 1");
+        else if (number ==2) Console.WriteLine("Number is 2");
+        else if (number <= 0) Console.WriteLine("Number must be more than 0");
+        else
+        {
+            int count = 0;
+            for (int i = 3; i<=number/2; i++)
+            {
+                if (number%i==0) count++;
+            }
+            if (count == 0) Console.WriteLine("Number is prime");
+            else Console.WriteLine("Number is not prime");
+        }
+    }
+    public static void Factorial(this int number)
+    {
+        if (number == 0 && number == 1) Console.WriteLine($"Result : 1");
+        else if (number < 0) Console.WriteLine("Enter number more than 0");
+        else
+        {
+            int temp = 1;
+            for (int i = number; i>1; i--)
+            {
+                temp *= i;
+            }
+            Console.WriteLine($"Result : {temp}");
+        }
+    }
+    public static double Fahrenheit(this int number)
+    {
+        return (number * 1.8) + 32;
+    }
+    public static int Celcius(this int number)
+    {
+        return (int)Math.Round((number - 32) / 1.8);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using ConsoleApp7;
+
+Console.Write("Eded daxil edin : ");
+int number = int.Parse(Console.ReadLine());
+//number.IsPrime();
+number.Factorial();
+//Console.WriteLine(number.Fahrenheit());
+//Console.WriteLine(number.Celcius());
